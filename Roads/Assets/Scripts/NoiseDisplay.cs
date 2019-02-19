f1e0af2cb480b4a346b828ff1eeff2d1313c48ae
Ryan <rcjones9@hotmail.com>
2019-02-19 16:56:08 +0000

    Population Areas marked

---
+++ Roads/Assets/Scripts/NoiseDisplay.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseDisplay : MonoBehaviour {
+
+    public Renderer textureRender;
+    public int mapWidth;
+    public int mapHeight;
+
+    [Range(1f, 50f)]
+    public float noiseScale;
+
+    private float[,] noiseMap;
+
+    public List<Vector2> PopDensityLocations = new List<Vector2>();
+
+    private Color popLowerBound = new Color(0.95f, 0.95f, 0.95f);
+
+    public void Update()
+    {
+        float[,] tempNoiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        if (tempNoiseMap != noiseMap)
+        {
+            noiseMap = tempNoiseMap;
+            
+            DrawNoiseMap(noiseMap);
+        }
+    }
+
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        PopDensityLocations.Clear();
+
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colourMap = new Color[width * height];
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+
+                if(colourMap[y * width + x].r >= popLowerBound.r)
+                {
+                    PopDensityLocations.Add(new Vector2(x,y));
+                }
+            }
+        }
+        texture.SetPixels(colourMap);
+        texture.Apply();
+
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(width, 1, height);
+
+    }
+
+    public List<Vector2> getPopLocations()
+    {
+        return PopDensityLocations;
+    }
+
+
+}
---
+++ Roads/Assets/Scripts/PopulationPoints.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationPoints : MonoBehaviour {
+
+    public NoiseDisplay noiseGeneration;
+
+    public GameObject populationHubObject;
+
+    public List<GameObject> populationHotSpots = new List<GameObject>();
+
+    public List<Vector2> popList = new List<Vector2>();
+
+
+    // Update is called once per frame
+    void Update () {
+        var tempList = noiseGeneration.getPopLocations();
+
+        if (checkLists(popList, tempList) == false)
+        {
+            popList = tempList;
+            hotspotGeneration(popList);
+        }
+	}
+
+    bool checkLists(List<Vector2> list1, List<Vector2> list2)
+    {
+        if ((list1 == null) && (list2 == null))
+            return true;
+        else if ((list1 == null) || (list2 == null))
+            return false;
+
+        if (list1.Count != list2.Count)
+            return false;
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (list1[i] != list2[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    void hotspotGeneration(List<Vector2> hotspots)
+    {
+
+        if (populationHotSpots.Count > 0)
+        {
+            foreach (GameObject hotSpot in populationHotSpots)
+            {
+                Object.Destroy(hotSpot);
+            }
+
+            populationHotSpots.Clear();
+        }
+        
+        foreach (Vector2 location in popList)
+        {
+            Vector3 pos = new Vector3(location.x, 1, location.y);
+            GameObject popHub = GameObject.Instantiate(populationHubObject, pos, this.transform.rotation, this.transform);
+            populationHotSpots.Add(popHub);
+        }
+
+        return;
+    }
+}
